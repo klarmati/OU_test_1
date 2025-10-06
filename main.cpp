06a21d10da162bef72c12536a468183407364736
klarmati <lazecki.mateusz@gmail.com>
2025-10-06 11:26:34 +0200

    01 zmiana policz

--- main.cpp
+++ main.cpp
@@ -1,21 +1,7 @@
 #include <iostream>
 
-
-void bump(int& variable) {
-    ++variable;
-}
-
 int main() {
     std::cout << "Hello World!\n";
-    int a{20};
-    int& ref = a;
-
-    while (a<200) {
-
-        bump(ref);
-        std::cout << a << '\n';
-    }
-
 
     return 0;
 }
